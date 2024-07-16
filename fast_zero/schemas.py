a94b2b99e1f9dbfacf1d54ed0132c3f36f527934
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2024-07-16 10:17:42 -0300

    feat userdb userpublic

--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -9,3 +9,13 @@ class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserDB(UserSchema):
+    id: int
